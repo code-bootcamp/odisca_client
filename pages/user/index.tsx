9fedd39a2aee569f363c09128dbafa64c3940d4e
june082 <115727989+june082@users.noreply.github.com>
2023-05-15 15:18:48 +0900

    feat: add user mainpage

--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -3,5 +3,9 @@
 import UserMain from "../../src/components/units/user/main/userMain.index";
 
 export default function UserMainPage(): JSX.Element {
-  return <UserMain />;
+  return (
+    <>
+      <UserMain />
+    </>
+  );
 }
