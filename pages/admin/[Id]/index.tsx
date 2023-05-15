0dc73d5424fb57110aaaac02b80a36ea1dcb7270
jiiyaeya <jiiyaeya@naver.com>
2023-05-15 11:14:33 +0900

    feat/edit adminWrite

--- pages/admin/[Id]/index.tsx
+++ pages/admin/[Id]/index.tsx
@@ -1,7 +1,1 @@
 // 유저 카페 상세페이지 테스트중
-
-import AdminDetail from "../../../src/components/units/admin/datail/AdminDetail.index";
-
-export default function AdminDetailPage(): JSX.Element {
-  return <AdminDetail />;
-}
