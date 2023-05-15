a5236b5a01e3bb7c8cc7f412195391e573a03baf
jiiyaeya <jiiyaeya@naver.com>
2023-05-15 11:13:24 +0900

    feat:edit adminWrite

--- pages/user/[Id]/index.tsx
+++ pages/user/[Id]/index.tsx
@@ -1,7 +1,1 @@
 // 유저 카페 상세페이지
-
-import UserDetail from "../../../src/components/units/user/datail/UserDetail.index";
-
-export default function UserDetailPage(): JSX.Element {
-  return <UserDetail />;
-}
