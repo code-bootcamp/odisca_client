81e298869c5aa1a1a5e05a4c14d8b5d9590a06ee
tjdgoz <125108800+tjdgoz@users.noreply.github.com>
2023-05-18 14:26:01 +0900

    checkvalification

--- src/components/commons/emailValidation/emailValidation.index.tsx
+++ src/components/commons/emailValidation/emailValidation.index.tsx
@@ -1,10 +1,23 @@
 // 이메일 인증번호 보내주는 페이지(모달안에 들어갈 내용)
 
+import { Modal } from "antd";
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { wrapFormAsync } from "../../../commons/libraries/asyncFunc";
+import { useMutationCheckVerificationCode } from "../hooks/mutations/useMutationCheckVerification";
 import * as S from "./emailValidation.styles";
 
-export default function EmailValidationPage(): JSX.Element {
-  const [seconds, setSeconds] = useState(180); // 초기 값으로 180초(3분)를 설정
+interface IFormValidationData {
+  verificationCode: string;
+}
+
+export default function EmailValidationPage(props): JSX.Element {
+  const [checkVerificationCode] = useMutationCheckVerificationCode();
+  const { register, handleSubmit } = useForm<IFormValidationData>({
+    // resolver: yupResolver(signUpSchema),
+    mode: "onChange",
+  });
+  const [seconds, setSeconds] = useState(180); // 인증시간 180초(3분)
 
   useEffect(() => {
     // 1초마다 타이머를 감소
@@ -18,6 +31,33 @@ export default function EmailValidationPage(): JSX.Element {
     };
   }, []);
 
+  useEffect(() => {
+    if (seconds === 0) {
+      // 타이머가 만료되면 알림을 표시하고 모달을 닫음
+      Modal.error({
+        content: "인증시간이 만료되었습니다. 인증을 다시 진행해주세요!",
+      });
+      props.handleCancel(); // src/components//user/
+    }
+  }, [seconds]);
+
+  const onClickCheckVerification = async (
+    data: IFormValidationData
+  ): Promise<void> => {
+    try {
+      const checkVerificationResult = checkVerificationCode({
+        variables: { verificationCode: data.verificationCode },
+      });
+      console.log(checkVerificationResult);
+      console.log(data.verificationCode);
+    } catch (error) {
+      if (error instanceof Error)
+        Modal.error({
+          content: error.message,
+        });
+    }
+  };
+
   // 남은 시간을 분과 초로 변환
   const remainingMinutes = Math.floor(seconds / 60);
   const remainingSeconds = seconds % 60;
@@ -26,7 +66,10 @@ export default function EmailValidationPage(): JSX.Element {
       <S.Wrapper>
         <S.Title>이메일 확인 후 인증번호를 입력해주세요.</S.Title>
         <S.ValidationWrapper>
-          <S.Input placeholder="인증번호 입력"></S.Input>
+          <S.Input
+            placeholder="인증번호 입력"
+            {...register("verificationCode")}
+          />
           <S.Timer>{`${remainingMinutes}:${remainingSeconds
             .toString()
             .padStart(2, "0")}`}</S.Timer>
@@ -36,7 +79,12 @@ export default function EmailValidationPage(): JSX.Element {
           <S.Error>인증번호가 다릅니다. 다시 진행해주세요!</S.Error>
         </S.ErrorWrapper>
         <S.BtnWrapper>
-          <S.SubmitBtn type="button">확인</S.SubmitBtn>
+          <S.SubmitBtn
+            type="button"
+            onClick={wrapFormAsync(handleSubmit(onClickCheckVerification))}
+          >
+            확인
+          </S.SubmitBtn>
         </S.BtnWrapper>
       </S.Wrapper>
     </>
