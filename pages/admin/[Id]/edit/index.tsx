518efb7af05048c795bc58307729b3995b99c142
june082 <115727989+june082@users.noreply.github.com>
2023-05-18 17:16:27 +0900

    feat: Add admin editpage

---
+++ pages/admin/[Id]/edit/index.tsx
@@ -0,0 +1,5 @@
+import AdminWrite from "../../../../src/components/units/admin/write/adminWrite.index";
+
+export default function AdminEditPage(): JSX.Element {
+  return <AdminWrite isEdit={true} />;
+}
