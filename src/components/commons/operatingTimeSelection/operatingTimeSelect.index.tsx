99c08fdabdc9f73dda824cb80facd22075be9ad7
june082 <115727989+june082@users.noreply.github.com>
2023-05-19 20:23:13 +0900

    feat: Edit admin-create-page

--- src/components/commons/operatingTimeSelection/operatingTimeSelect.index.tsx
+++ src/components/commons/operatingTimeSelection/operatingTimeSelect.index.tsx
@@ -9,36 +9,78 @@ const OperatingTimeSpan = styled.span`
   margin: 0px 8px;
 `;
 
-export default function OperatingTime(): JSX.Element {
+export default function OperatingTime(props): JSX.Element {
   return (
     <>
-      <SelectOperatingTime>
+      <SelectOperatingTime onChange={props.onChangeSelectOpenTime}>
         <option>영업시작시간</option>
-        <option>00:00</option>
-        <option>01:00</option>
-        <option>02:00</option>
-        <option>03:00</option>
-        <option>04:00</option>
-        <option>05:00</option>
-        <option>06:00</option>
-        <option>07:00</option>
-        <option>08:00</option>
-        <option>09:00</option>
-        <option>10:00</option>
-        <option>11:00</option>
+        <option value={"00:00"} label={"00:00"}>
+          00:00
+        </option>
+        <option value={"01:00"} label={"01:00"}>
+          01:00
+        </option>
+        <option value={"02:00"} label={"02:00"}>
+          02:00
+        </option>
+        <option value={"03:00"} label={"03:00"}>
+          03:00
+        </option>
+        <option value={"04:00"} label={"04:00"}>
+          04:00
+        </option>
+        <option value={"05:00"} label={"05:00"}>
+          05:00
+        </option>
+        <option value={"06:00"} label={"06:00"}>
+          06:00
+        </option>
+        <option value={"07:00"} label={"07:00"}>
+          07:00
+        </option>
+        <option value={"08:00"} label={"08:00"}>
+          08:00
+        </option>
+        <option value={"09:00"} label={"09:00"}>
+          09:00
+        </option>
+        <option value={"10:00"} label={"10:00"}>
+          10:00
+        </option>
+        <option value={"11:00"} label={"11:00"}>
+          11:00
+        </option>
       </SelectOperatingTime>
       <OperatingTimeSpan>-</OperatingTimeSpan>
-      <SelectOperatingTime>
+      <SelectOperatingTime onChange={props.onChangeSelectCloseTime}>
         <option>영업종료시간</option>
-        <option>18:00</option>
-        <option>19:00</option>
-        <option>20:00</option>
-        <option>21:00</option>
-        <option>22:00</option>
-        <option>23:00</option>
-        <option>00:00</option>
-        <option>01:00</option>
-        <option>02:00</option>
+        <option value={"18:00"} label={"18:00"}>
+          18:00
+        </option>
+        <option value={"19:00"} label={"19:00"}>
+          19:00
+        </option>
+        <option value={"20:00"} label={"20:00"}>
+          20:00
+        </option>
+        <option value={"21:00"} label={"21:00"}>
+          21:00
+        </option>
+        <option value={"22:00"} label={"22:00"}>
+          22:00
+        </option>
+        <option value={"23:00"} label={"23:00"}>
+          23:00
+        </option>
+        <option value={"24:00"} label={"24:00"}>
+          24:00
+        </option>
+        <option value={"01:00"} label={"01:00"}>
+          01:00
+        </option>
+        <option value={"02:00"} label={"02:00"}>
+          02:00
+        </option>
       </SelectOperatingTime>
     </>
   );
