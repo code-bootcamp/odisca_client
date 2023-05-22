8161acd4d0470f0c407f1c8bc98bd31c4b1cea79
jiiyaeya <jiiyaeya@naver.com>
2023-05-22 15:57:30 +0900

    feat:edit userMain

--- src/components/commons/hooks/customs/useMap.tsx
+++ src/components/commons/hooks/customs/useMap.tsx
@@ -1,13 +1,9 @@
-import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 declare const window: typeof globalThis & {
   kakao: any;
 };
 export default function Map(): JSX.Element {
-  const router = useRouter();
-  const infoWindows = [];
-
   useEffect(() => {
     const script = document.createElement("script");
     script.src =
@@ -24,94 +20,9 @@ export default function Map(): JSX.Element {
         };
         const map = new window.kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
         console.log(map);
-        const positions = [
-          {
-            content:
-              '<div class="wrap">' +
-              '    <div class="info" style="padding:10px">' +
-              '        <div class="title" style="display: flex; flex-direction: row; justify-content: space-between; align-items:center;">' +
-              '<div style="font-size:20px;">어디스카페</div>' +
-              '            <div style="cursor:pointer; padding:10px" id="closeBtn">X</div>' +
-              "        </div>" +
-              '        <div class="body" style="display:flex;">' +
-              '            <div class="img">' +
-              '                <img src="https://cfile181.uf.daum.net/image/250649365602043421936D" width="73" height="70" id="image" style="cursor:pointer;">' +
-              "           </div>" +
-              '            <div class="desc" style="margin-left:10px;">' +
-              '                <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>' +
-              "            </div>" +
-              "        </div>" +
-              "    </div>" +
-              "</div>",
-            latlng: new window.kakao.maps.LatLng(
-              37.5178135473914,
-              127.04128186524144
-            ),
-          },
-          {
-            content: "<div>생태연못</div>",
-            latlng: new window.kakao.maps.LatLng(
-              37.51293439357456,
-              127.05310473668925
-            ),
-          },
-          {
-            content: "<div>텃밭</div>",
-            latlng: new window.kakao.maps.LatLng(
-              37.51762206429312,
-              127.04733913230727
-            ),
-          },
-          {
-            content: "<div>근린공원</div>",
-            latlng: new window.kakao.maps.LatLng(
-              37.51429768317861,
-              127.04652828748588
-            ),
-          },
-        ];
-
-        // prettier-ignore
-        for (let i = 0; i < positions.length; i++) {
-          const marker = new window.kakao.maps.Marker({
-            map, 
-            position: positions[i].latlng, 
-          });
-          const infowindow = new window.kakao.maps.InfoWindow({
-            content: positions[i].content, 
-          });
-          infoWindows.push(infowindow);
-          window.kakao.maps.event.addListener(marker, "click", function () {
-            closeAllInfoWindows();
-            infowindow.setContent(positions[i].content);
-            infowindow.open(map, marker);
-          });
-
-          document.addEventListener("click", function (event) {
-            const target = event.target as HTMLElement;
-            if (target.matches("#closeBtn")) {
-              infowindow.close();
-            }
-          });
-         
-          document.addEventListener("click", function (event) {
-            const target = event.target as HTMLElement;
-            if (target.matches("#image")) {
-            void  router.push(`/user/login`)
-            }
-          });
-
-
-        }
       });
     };
   }, []);
-
-  const closeAllInfoWindows = (): void => {
-    infoWindows.forEach((infowindow) => {
-      infowindow.close();
-    });
-  };
 
   return <div id="map" style={{ width: "100%", height: "100%" }}></div>;
 }
--- src/components/commons/searchbars/01/SearchBar01.index.tsx
+++ src/components/commons/searchbars/01/SearchBar01.index.tsx
@@ -1,11 +1,6 @@
 import { useState } from "react";
 
 import * as S from "./SearchBar.styles";
-import { useRouter } from "next/router";
-
-declare const window: typeof globalThis & {
-  kakao: any;
-};
 
 type CityData = Record<string, string[]>;
 
@@ -19,18 +14,17 @@ const cityData:CityData = {
   ],
 };
 
-const cafeDatas = [
-  { district: "강남구", lon: 127.0495556, lat: 37.514575 },
-  { district: "구로구", lon: 126.8895972, lat: 37.49265 },
-  { district: "노원구", lon: 127.0583889, lat: 37.65146111 },
-  { district: "동대문구", lon: 127.0421417, lat: 37.571625 },
-  { district: "마포구", lon: 126.9105306, lat: 37.50965556 },
-];
+// const cafeDatas = [
+//   { district: "강남구", lon: 127.0495556, lat: 37.514575 },
+//   { district: "구로구", lon: 126.8895972, lat: 37.49265 },
+//   { district: "노원구", lon: 127.0583889, lat: 37.65146111 },
+//   { district: "동대문구", lon: 127.0421417, lat: 37.571625 },
+//   { district: "마포구", lon: 126.9105306, lat: 37.50965556 },
+// ];
 
 export default function SearchBar01(): JSX.Element {
   const [cities, setCities] = useState(cityData[provinceData[0]]);
   const [secondCity, setSecondCity] = useState(cityData[provinceData[0]][0]);
-  const router = useRouter();
 
   const handleProvinceChange = (value: string): void => {
     setCities(cityData[value]);
@@ -39,73 +33,6 @@ export default function SearchBar01(): JSX.Element {
 
   const onSecondCityChange = (value: string): void => {
     setSecondCity(value);
-    const selectedCafe = cafeDatas.find((city) => city.district === value);
-    if (selectedCafe !== undefined) {
-      const { lat, lon } = selectedCafe;
-      const locPosition = new window.kakao.maps.LatLng(lat, lon);
-      if (window.kakao.maps.Map !== undefined) {
-        const map = new window.kakao.maps.Map(document.getElementById("map"), {
-          center: locPosition,
-          level: 3,
-        });
-        map.panTo(locPosition);
-
-        const positions = cafeDatas
-          .filter((cafe) => cafe.district === value)
-          .map((cafe) => ({
-            content:
-              '<div class="wrap">' +
-              '    <div class="info" style="padding:10px">' +
-              '        <div class="title" style="display: flex; flex-direction: row; justify-content: space-between; align-items:center;">' +
-              '<div style="font-size:20px;">어디스카페</div>' +
-              '            <div style="cursor:pointer; padding:10px" id="closeBtn">X</div>' +
-              "        </div>" +
-              '        <div class="body" style="display:flex;">' +
-              '            <div class="img">' +
-              '                <img src="https://cfile181.uf.daum.net/image/250649365602043421936D" width="73" height="70" id="image" style="cursor:pointer;">' +
-              "           </div>" +
-              '            <div class="desc" style="margin-left:10px;">' +
-              '                <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>' +
-              "            </div>" +
-              "        </div>" +
-              "    </div>" +
-              "</div>",
-            latlng: new window.kakao.maps.LatLng(cafe.lat, cafe.lon),
-          }));
-        // prettier-ignore
-        for (let i = 0; i < positions.length; i++) {
-          const marker = new window.kakao.maps.Marker({
-            map, 
-            position: positions[i].latlng, 
-          });
-          const infowindow = new window.kakao.maps.InfoWindow({
-            content: positions[i].content, 
-          });
-
-          window.kakao.maps.event.addListener(marker, "click", function () {
-            infowindow.setContent(positions[i].content);
-            infowindow.open(map, marker);
-
-          });
-
-          document.addEventListener("click", function (event) {
-            const target = event.target as HTMLElement;
-            if (target.matches("#closeBtn")) {
-              infowindow.close();
-            }
-          });
-         
-          document.addEventListener("click", function (event) {
-            const target = event.target as HTMLElement;
-            if (target.matches("#image")) {
-            void  router.push(`/user/login`)
-            }
-          });
-
-
-        }
-      }
-    }
   };
 
   return (
