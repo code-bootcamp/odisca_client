c2d1322662b8c595a50caa4449ebac0cb5a29f45
tjdgoz <125108800+tjdgoz@users.noreply.github.com>
2023-05-24 16:06:04 +0900

    feat: Add createReview api

---
+++ src/components/commons/hooks/queries/useQueryFetchLoginReviews.tsx
@@ -0,0 +1,31 @@
+import { gql, useQuery, QueryResult } from "@apollo/client";
+import { IQuery, IVisit } from "../../../../commons/types/generated/types";
+
+interface IFetchUserReviewQueryResult
+  extends Omit<
+    QueryResult<Pick<IQuery, "fetchLoginReviewsByUserId">, IVisit>,
+    "refetch"
+  > {
+  refetch: () => Promise<void>;
+}
+
+export const FETCH_REVIEW = gql`
+  query {
+    fetchLoginReviewsByUserId {
+      review_id
+      review_content
+      review_createdAt
+    }
+  }
+`;
+
+export const useQueryFetchReview = (): IFetchUserReviewQueryResult => {
+  const query = useQuery<Pick<IQuery, "fetchLoginReviewsByUserId">, IVisit>(
+    FETCH_REVIEW
+  );
+  const refetch = async (): Promise<void> => {
+    await query.refetch();
+  };
+
+  return { ...query, refetch };
+};
--- src/components/commons/reviews/review.index.tsx
+++ src/components/commons/reviews/review.index.tsx
@@ -2,18 +2,21 @@ import { Modal } from "antd";
 import { useForm } from "react-hook-form";
 import { wrapFormAsync } from "../../../commons/libraries/asyncFunc";
 import { useMutationCreateReview } from "../hooks/mutations/useMutationCreateReview";
+import { useQueryFetchLoginUser } from "../hooks/queries/useQueryFetchLoginUser";
 import * as S from "./review.styles";
+import { getDate } from "../../../commons/libraries/utils";
+import { useQueryFetchReview } from "../hooks/queries/useQueryFetchLoginReviews";
 
 interface IFormReviewData {
   review_content: string;
-  // visit_id: string;
+  visit_id: string;
 }
 
 export default function Review(): JSX.Element {
   const [createLoginReview] = useMutationCreateReview();
-
+  const { data: reviewdata } = useQueryFetchReview();
+  const { data: fetchUserdata } = useQueryFetchLoginUser();
   const { register, handleSubmit } = useForm({
-    // resolver: yupResolver(schema),
     mode: "onChange",
   });
 
@@ -23,11 +26,14 @@ export default function Review(): JSX.Element {
         variables: {
           createReviewInput: {
             review_content: data.review_content,
-            // visit_id:
+            visit_id: reviewdata?.fetchLoginReviewsByUserId[0].review_content,
           },
         },
       });
       console.log(result);
+      Modal.success({
+        content: "",
+      });
     } catch (error) {
       if (error instanceof Error)
         Modal.error({
@@ -35,13 +41,18 @@ export default function Review(): JSX.Element {
         });
     }
   };
+
+  console.log(reviewdata?.fetchLoginReviewsByUserId[0].review_content, "댓글");
   return (
     <>
       <S.Wrapper>
         <S.Title>
-          <S.CafeName>어디스카페</S.CafeName>
-          <S.Seat>A16</S.Seat>
-          <S.VisitDate>2023.05.16</S.VisitDate>
+          <S.CafeName>
+            {fetchUserdata?.fetchLoginUser.visits[0].studyCafe.studyCafe_name}
+          </S.CafeName>
+          <S.VisitDate>
+            {getDate(fetchUserdata?.fetchLoginUser.visits[0].visit_createdAt)}
+          </S.VisitDate>
         </S.Title>
         <S.ImgWrapper>
           <S.CafeImg src="/cafeImg.jpeg"></S.CafeImg>
@@ -53,6 +64,9 @@ export default function Review(): JSX.Element {
           <S.ReviewInput
             {...register("review_content")}
             placeholder="무분별한 비방, 욕설 등 타인을 불쾌하게 하는 리뷰는 사전 통보 없이 삭제될 수 있습니다."
+            defaultValue={
+              reviewdata?.fetchLoginReviewsByUserId[0].review_content
+            }
           ></S.ReviewInput>
           <S.ReviewBtn>등록</S.ReviewBtn>
         </S.ReviewWrapper>
