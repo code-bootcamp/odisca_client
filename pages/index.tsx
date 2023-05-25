6d87eaf3aa04215636b78d8f7c2318f67e9ee9c8
tjdgoz <125108800+tjdgoz@users.noreply.github.com>
2023-05-25 23:44:02 +0900

    feat: Add landingPage-2-UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,6 @@ export default function LandingPage(): JSX.Element {
     <>
       <S.Wrapper>
         <S.First>
-          <S.Logo src="/logo_2.png" />
           <S.FirstTitleWrapper>
             <S.FirstTitle1 data-aos="fade-down">
               내 주변 스터디카페,
@@ -24,27 +23,28 @@ export default function LandingPage(): JSX.Element {
             <S.FirstTitle2 data-aos="fade-up" data-aos-delay="300">
               어디스카?
             </S.FirstTitle2>
-            <S.FirstImg></S.FirstImg>
+            <S.FirstImg src="/landing_1.svg"></S.FirstImg>
           </S.FirstTitleWrapper>
-          {/* <Carousel /> */}
         </S.First>
+
         <S.Second>
           <S.SecondWrapper>
-            <S.TitleWrapper>
-              <S.SecondTitle1>1초가 아까운 시험기간,</S.SecondTitle1>
-              <S.SecondTitle1>오랜시간 줄서서 기다리지 마세요.</S.SecondTitle1>
+            <S.TitleWrapper data-aos="fade-right">
+              <S.SecondTitle1>
+                {`1초가 아까운 시험기간,
+                줄서지 말고 미리 예약하세요.`}
+              </S.SecondTitle1>
+
               <S.SecondTitle2>
-                시험기간, 스터디 카페 자리가 없어 발길을 돌리신 적 있으신가요?
-              </S.SecondTitle2>
-              <S.SecondTitle2>
-                어디스카에서는 실시간으로 좌석 조회 및 예약까지 가능합니다.
-              </S.SecondTitle2>
-              <S.SecondTitle2>
-                내가 원하는 좌석, 어디스카에서 미리 구매하세요!
+                {`시험기간, 스터디 카페 자리가 없어 발길을 돌리신 적 있으신가요?
+                  어디스카에서는 실시간으로 좌석 조회 및 예약까지 가능합니다.
+                  내가 원하는 좌석, 어디스카에서 미리 구매하세요!
+                `}
               </S.SecondTitle2>
             </S.TitleWrapper>
-
-            <S.SecondImg></S.SecondImg>
+            <S.SecondImgWrapper data-aos="fade-left">
+              <S.SecondImg src="/landing_2.svg"></S.SecondImg>
+            </S.SecondImgWrapper>
           </S.SecondWrapper>
         </S.Second>
         <S.Third></S.Third>
