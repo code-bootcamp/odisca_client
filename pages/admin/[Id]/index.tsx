7f6cf5b412a78612147e54f2cb798b977a949f06
jiiyaeya <jiiyaeya@naver.com>
2023-05-26 22:21:29 +0900

    feat:edit pages

--- pages/admin/[Id]/index.tsx
+++ pages/admin/[Id]/index.tsx
@@ -1,4 +1,4 @@
-// 유저 카페 상세페이지 테스트중
+// 유저 카페상세페이지
 import AdminDetail from "../../../src/components/units/admin/datail/adminDetail.index";
 
 export default function adminDetailPage(): JSX.Element {
