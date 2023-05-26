3e619013da534a5898087e4819b85f0487c8699c
tjdgoz <125108800+tjdgoz@users.noreply.github.com>
2023-05-26 15:03:34 +0900

    feat: Add lendingPage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
-import Carousel from "../src/commons/landingPageStyles/carousel";
 import * as S from "../src/commons/landingPageStyles/index";
 
 export default function LandingPage(): JSX.Element {
   useEffect(() => {
     AOS.init({
       duration: 1300,
+      easing: "ease-in-out",
     });
     AOS.refresh();
   });
@@ -47,8 +47,58 @@ export default function LandingPage(): JSX.Element {
             </S.SecondImgWrapper>
           </S.SecondWrapper>
         </S.Second>
-        <S.Third></S.Third>
-        <S.Fourth></S.Fourth>
+
+        <S.Third>
+          <S.ThirdWrapper>
+            <S.Top>
+              <S.TopLeftImg data-aos="fade-in" data-aos-delay="300">
+                <S.ThirdTopImg src="/cafeImg.jpeg"></S.ThirdTopImg>
+              </S.TopLeftImg>
+              <S.TopContentsWrapper data-aos="zoom-in" data-aos-delay="0">
+                <S.TopRightContents1>
+                  {`충전 한번이면 어디서나 사용 가능한 포인트!`}
+                </S.TopRightContents1>
+                <S.TopRightContents2>
+                  {`비싼 시간제로 구매하긴 부담스럽고 다 쓸 수 있을지 애매할 때 
+                  사용하고 싶은 만큼의 포인트만 결제할 수 있어요.
+                  어디스카 포인트 한번 충전이면, 모든 스터디카페 이용이 가능합니다!
+                `}
+                </S.TopRightContents2>
+              </S.TopContentsWrapper>
+            </S.Top>
+            <S.Bottom>
+              <S.BottomContentsWrapper data-aos="zoom-in" data-aos-delay="0">
+                <S.BottomLeftContents1>
+                  {`스터디 카페 찾기, 어디스카에게 맡겨주세요.`}
+                </S.BottomLeftContents1>
+                <S.BottomLeftContents2>
+                  {`이용하고싶은 지역을 검색하면 
+                  주변 스터디카페의 위치, 실시간 좌석, 그리고 실제 이용자들의 리뷰를 
+                  한번에 확인 가능합니다.
+                  `}
+                </S.BottomLeftContents2>
+              </S.BottomContentsWrapper>
+
+              <S.BottomRightImg data-aos="fade-in" data-aos-delay="0">
+                <S.ThirdBottomImg src="/cafeImg.jpeg"></S.ThirdBottomImg>
+              </S.BottomRightImg>
+            </S.Bottom>
+          </S.ThirdWrapper>
+        </S.Third>
+
+        <S.Fourth>
+          <S.FourthTop>
+            <S.TopTiTle>어디스카 어쩌구저쩌구</S.TopTiTle>
+          </S.FourthTop>
+          <S.FourthBottom>
+            <S.FourthLeft>
+              <S.GoAdminBtn>관리자페이지</S.GoAdminBtn>
+            </S.FourthLeft>
+            <S.FourthRight>
+              <S.GoUserBtn>유저페이지</S.GoUserBtn>
+            </S.FourthRight>
+          </S.FourthBottom>
+        </S.Fourth>
       </S.Wrapper>
     </>
   );
--- src/commons/landingPageStyles/index.ts
+++ src/commons/landingPageStyles/index.ts
@@ -5,7 +5,6 @@ import { mediaQueries } from "../../components/commons/media/mediaQueries";
 
 export const Wrapper = styled.div`
   width: 100vw;
-  /* height: 500vh; */
   display: flex;
   flex-direction: column;
 `;
@@ -150,6 +149,9 @@ export const TitleWrapper = styled.div`
   left: 20vw;
   top: 30vh;
   z-index: 3;
+  ${mediaQueries("macBook")} {
+    left: 15vw;
+  }
 `;
 
 export const SecondTitle1 = styled.h1`
@@ -158,6 +160,9 @@ export const SecondTitle1 = styled.h1`
   width: 100vw;
   white-space: pre-line;
   margin-bottom: 3vh;
+  ${mediaQueries("macBook")} {
+    font-size: 33px;
+  }
 `;
 
 export const SecondTitle2 = styled.p`
@@ -165,11 +170,14 @@ export const SecondTitle2 = styled.p`
   font-size: 20px;
   width: 100vw;
   white-space: pre-line;
+  ${mediaQueries("macBook")} {
+    font-size: 19px;
+  }
 `;
 
 export const SecondImgWrapper = styled.div`
   position: absolute;
-  left: 800px;
+  left: 43vw;
   z-index: 2;
 `;
 
@@ -177,6 +185,10 @@ export const SecondImg = styled.img`
   width: 700px;
   height: 700px;
   background-color: #bdbdbd;
+  ${mediaQueries("macBook")} {
+    width: 600px;
+    height: 600px;
+  }
 `;
 
 // ====== 세번째 랜딩페이지 ====== //
@@ -185,10 +197,162 @@ export const Third = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: #40e0d0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
 `;
+
+export const ThirdWrapper = styled.div`
+  width: 80vw;
+  display: flex;
+  flex-direction: column;
+  z-index: 0;
+  position: relative;
+`;
+
+export const Top = styled.div`
+  width: 100%;
+  height: 50vh;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const TopLeftImg = styled.div`
+  width: 50%;
+  height: 50vh;
+`;
+
+export const ThirdTopImg = styled.img`
+  width: 100%;
+  height: 50vh;
+  opacity: 1;
+`;
+
+export const TopContentsWrapper = styled.div`
+  padding-top: 17vh;
+  padding-left: 2vw;
+  width: 50%;
+  ${mediaQueries("macBook")} {
+    padding-top: 15vh;
+  }
+`;
+
+export const TopRightContents1 = styled.h1`
+  font-size: 40px;
+
+  color: #fff;
+  margin-bottom: 3vh;
+  ${mediaQueries("macBook")} {
+    font-size: 31px;
+  }
+`;
+
+export const TopRightContents2 = styled.div`
+  white-space: pre-line;
+  font-size: 20px;
+  color: #fff;
+  ${mediaQueries("macBook")} {
+    font-size: 19px;
+  }
+`;
+
+export const Bottom = styled.div`
+  width: 100%;
+  height: 50vh;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const BottomContentsWrapper = styled.div`
+  padding-top: 17vh;
+  padding-left: 2vw;
+  width: 50%;
+  ${mediaQueries("macBook")} {
+    padding-top: 15vh;
+  }
+`;
+
+export const BottomLeftContents1 = styled.h1`
+  font-size: 40px;
+  color: #fff;
+  white-space: pre-line;
+  margin-bottom: 3vh;
+  ${mediaQueries("macBook")} {
+    font-size: 31px;
+  }
+`;
+
+export const BottomLeftContents2 = styled.div`
+  font-size: 20px;
+  color: #fff;
+  white-space: pre-line;
+  ${mediaQueries("macBook")} {
+    font-size: 19px;
+  }
+`;
+
+export const BottomRightImg = styled.div`
+  width: 50%;
+`;
+
+export const ThirdBottomImg = styled.img`
+  width: 100%;
+  height: 50vh;
+  opacity: 1;
+`;
+
+// ====== 네번째 랜딩페이지 ====== //
 
 export const Fourth = styled.div`
   width: 100vw;
   height: 100vh;
-  background-color: #fff;
+  background-color: #4f4f4f;
+`;
+
+export const FourthTop = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 15vh;
+`;
+
+export const TopTiTle = styled.div`
+  color: #fff;
+  font-size: 50px;
+  font-weight: 600;
+`;
+
+export const FourthLeft = styled.div``;
+
+export const FourthBottom = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 20vh;
+`;
+
+export const FourthRight = styled.div``;
+
+export const GoAdminBtn = styled.button`
+  width: 180px;
+  height: 50px;
+  background-color: #4f4f4f;
+  border: 1px solid #40e0d0;
+  color: #40e0d0;
+  border-radius: 30px;
+  font-size: 23px;
+  font-weight: 500;
+  margin-right: 1vw;
+`;
+
+export const GoUserBtn = styled.button`
+  width: 180px;
+  height: 50px;
+  background-color: #40e0d0;
+  border: none;
+  color: #fff;
+  border-radius: 30px;
+  font-size: 23px;
+  font-weight: 500;
 `;
