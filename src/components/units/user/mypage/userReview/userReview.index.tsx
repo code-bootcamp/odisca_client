b5eaf58dee1b7108195c68b00dcaced374ca8ef3
tjdgoz <125108800+tjdgoz@users.noreply.github.com>
2023-06-16 15:30:26 +0900

    feat: Add ReviewUI

--- src/components/units/user/mypage/userReview/userReview.index.tsx
+++ src/components/units/user/mypage/userReview/userReview.index.tsx
@@ -1,22 +1,62 @@
 import { useQueryFetchReview } from "../../../../commons/hooks/queries/useQueryFetchLoginReviews";
 import * as S from "./userReview.styles";
+import { getDate } from "../../../../../commons/libraries/utils";
+import UseModal from "../../../../commons/hooks/customs/useModal";
+import { useState } from "react";
+import Review from "../../../../commons/reviews/review.index";
 
 export default function UserReview(): JSX.Element {
   const { data } = useQueryFetchReview();
+  const { showModal, handleOk, handleCancel, isModalOpen } = UseModal();
+  const [Ind, setInd] = useState(0);
+  const [vId, setVId] = useState("");
+
+  const onClickSetIndex = (i: number, id: string) => (): void => {
+    setInd(i);
+    setVId(id);
+    showModal();
+  };
 
   return (
-    <S.Wrapper>
+    <>
       {data?.fetchLoginReviewsByUserId.map((el) => (
         <div key={el.review_id}>
-          <S.CafeName>{el.studyCafe?.studyCafe_name}</S.CafeName>
-          <S.CafeName>{el.review_content}</S.CafeName>
-          {el.studyCafe?.images.map((image) =>
-            image.image_isMain ? (
-              <S.CafeNameI key={image.image_url} src={image.image_url} />
-            ) : null
-          )}
+          <S.Wrapper>
+            <S.LeftWrapper>
+              <S.CafeName>{el.studyCafe?.studyCafe_name}</S.CafeName>
+              <S.ReviewContent>{el.review_content}</S.ReviewContent>
+              <S.BtnWrapper>
+                <S.ReviewDate>{getDate(el.review_createdAt)}</S.ReviewDate>
+              </S.BtnWrapper>
+            </S.LeftWrapper>
+            <S.RightWrapper>
+              {el.studyCafe?.images.map((image) =>
+                image.image_isMain ? (
+                  <S.CafeImg key={image.image_url} src={image.image_url} />
+                ) : null
+              )}
+
+              <S.EditReviewBtn
+                onClick={onClickSetIndex(el?.index, el?.visit?.visit_id)}
+              >
+                리뷰수정
+              </S.EditReviewBtn>
+              {isModalOpen !== false && (
+                <S.ReviewModal
+                  okButtonProps={{ style: { display: "none" } }}
+                  cancelButtonProps={{ style: { display: "none" } }}
+                  open={isModalOpen}
+                  onOk={handleOk}
+                  onCancel={handleCancel}
+                >
+                  <Review handleCancel={handleCancel} index={Ind} vId={vId} />
+                </S.ReviewModal>
+              )}
+            </S.RightWrapper>
+          </S.Wrapper>
+          <S.Line></S.Line>
         </div>
       ))}
-    </S.Wrapper>
+    </>
   );
 }
--- src/components/units/user/mypage/userReview/userReview.styles.ts
+++ src/components/units/user/mypage/userReview/userReview.styles.ts
@@ -1,12 +1,80 @@
 import styled from "@emotion/styled";
+import { Modal } from "antd";
+import { mediaQueries } from "../../../../commons/media/mediaQueries";
 
 export const Wrapper = styled.div`
   width: 1000px;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  margin: 15px 0 15px 40px;
+`;
+
+export const Line = styled.div`
+  border-top: 1px solid #e7e7e7;
+  width: 1000px;
   margin: 0 auto;
 `;
 
-export const CafeName = styled.a``;
+export const LeftWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: calc(1000px - 150px);
+`;
 
-export const CafeNameI = styled.img``;
+export const BtnWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-end;
+`;
+
+export const ReviewContent = styled.a`
+  color: #4f4f4f;
+  margin-bottom: 10px;
+`;
+
+export const ReviewDate = styled.a`
+  color: #bdbdbd;
+  font-size: 15px;
+`;
+
+export const CafeName = styled.a`
+  font-size: 21px;
+  font-weight: 500;
+  margin-bottom: 35px;
+  margin-top: 10px;
+`;
+
+export const RightWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CafeImg = styled.img`
+  width: 110px;
+  height: 110px;
+`;
+
+export const EditReviewBtn = styled.button`
+  background-color: #ffffff;
+  border-radius: 10px;
+  color: #40e0d0;
+  border: 1px solid #40e0d0;
+  margin-top: 5px;
+`;
+
+export const ReviewModal = styled(Modal)`
+  .ant-modal-content {
+    width: 800px;
+    height: 900px;
+    left: -6vw;
+    ${mediaQueries("macBook")} {
+      width: 700px;
+      left: -3vw;
+    }
+  }
+
+  .ant-modal-body {
+  }
+`;
